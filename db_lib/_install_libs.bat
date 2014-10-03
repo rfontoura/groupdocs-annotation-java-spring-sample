180340b356a89cd50cf031f428e49885b11550ae
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2014-10-03 14:57:16 +0300

    ANNOTATIONJAVA-462: Update Spring sample to work with new save functional Removed no need library Updated CustomDatabaseConnector

--- db_lib/_install_libs.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo Installing MS SQL Server JDBC library...
-
-call mvn install:install-file -Dfile=sqljdbc4.jar -DgroupId=com.microsoft.sqlserver -DartifactId=sqljdbc4 -Dversion=4.0 -Dpackaging=jar
-
-pause
--- src/main/java/com/groupdocs/connector/CustomDatabaseConnector.java
+++ src/main/java/com/groupdocs/connector/CustomDatabaseConnector.java
@@ -1,81 +1,44 @@
 package com.groupdocs.connector;
 
-import com.groupdocs.annotation.connector.StorageType;
 import com.groupdocs.annotation.connector.db.AbstractDatabaseConnector;
-import com.groupdocs.annotation.connector.db.IDatabaseConnector;
-import com.groupdocs.config.ApplicationConfig;
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.groupdocs.annotation.exception.AnnotationException;
 import com.j256.ormlite.support.ConnectionSource;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
- * Custom connector for MySQL database
- * GroupDocs.Annotation have embedded MySQL connector
- * So this connector just example
+ * Custom connector for MySQL database.
+ * GroupDocs.Annotation have embedded MySQL connector.
+ * So this connector just example.
  *
  * @author Aleksey Permyakov (09.09.2014)
  */
 public class CustomDatabaseConnector extends AbstractDatabaseConnector {
     private static final String CONNECTION_STRING = "jdbc:mysql://%s:%d/%s?user=%s&password=%s";
-
-    private String dbDriver;
-    private String dbServer;
-    private int dbPort;
-    private String dbName;
-    private String dbUsername;
-    private String dbPassword;
+    private static final String DEFAULT_DATABASE_DRIVER = com.mysql.jdbc.Driver.class.getName();
 
     /**
-     * Create custom database connector
-     * @param dbDriver      Database driver (com.mysql.jdbc.Driver)
-     * @param dbServer      Database server (192.168.204.128)
-     * @param dbPort        Database port (1433)
-     * @param dbName        Database name (AnnotationDB)
-     * @param dbUsername    Database user name
-     * @param dbPassword    Database user password
+     * Create database connector
+     *
+     * @param dbServer   database server
+     * @param dbPort     database port
+     * @param dbName     database name
+     * @param dbUsername database user name
+     * @param dbPassword database user password
      */
-    public CustomDatabaseConnector(String dbDriver, String dbServer, int dbPort, String dbName, String dbUsername, String dbPassword) {
-        this.dbDriver = dbDriver;
-        this.dbServer = dbServer;
-        this.dbPort = dbPort;
-        this.dbName = dbName;
-        this.dbUsername = dbUsername;
-        this.dbPassword = dbPassword;
+    public CustomDatabaseConnector(String dbServer, int dbPort, String dbName, String dbUsername, String dbPassword) throws AnnotationException {
+        this(DEFAULT_DATABASE_DRIVER, dbServer, dbPort, dbName, dbUsername, dbPassword);
     }
 
-    @Override
-    public ConnectionSource getConnection() {
-        return getConnection(dbDriver, String.format(CONNECTION_STRING, dbServer, dbPort, dbName, dbUsername, dbPassword));
-    }
-
-    public void setDbDriver(String dbDriver) {
-        this.dbDriver = dbDriver;
-    }
-
-    public void setDbServer(String dbServer) {
-        this.dbServer = dbServer;
-    }
-
-    public void setDbPort(int dbPort) {
-        this.dbPort = dbPort;
-    }
-
-    public void setDbName(String dbName) {
-        this.dbName = dbName;
-    }
-
-    public void setDbUsername(String dbUsername) {
-        this.dbUsername = dbUsername;
-    }
-
-    public void setDbPassword(String dbPassword) {
-        this.dbPassword = dbPassword;
-    }
-
-    @Override
-    public StorageType getStorageType() {
-        return StorageType.CUSTOM;
+    /**
+     * Create database connector
+     *
+     * @param dbDriver   database driver class name
+     * @param dbServer   database server
+     * @param dbPort     database port
+     * @param dbName     database name
+     * @param dbUsername database user name
+     * @param dbPassword database user password
+     */
+    public CustomDatabaseConnector(String dbDriver, String dbServer, int dbPort, String dbName, String dbUsername, String dbPassword) throws AnnotationException {
+        super(dbDriver, String.format(CONNECTION_STRING, dbServer, dbPort, dbName, dbUsername, dbPassword));
     }
 }
